c82ffebb4cbc2f5173fef7c9f26b1f4fb3422f37
Vlad <Vlad@VLAD>
2023-10-31 11:57:09 +0500

    Vlad commit

---
+++ Test3/Window2vlad.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2vlad.xaml
+    /// </summary>
+    public partial class Window2vlad : Window
+    {
+        public Window2vlad()
+        {
+            InitializeComponent();
+        }
+    }
+}
